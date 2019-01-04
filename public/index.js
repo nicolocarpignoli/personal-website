324f77b7562526e35198915d87386245ed323e53
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2019-01-04 09:55:03 +0100

    fixed public

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 // start server!
 app.listen((process.env.PORT || 5000));
