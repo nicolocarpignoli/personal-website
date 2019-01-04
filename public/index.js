8ad4c3928856e14757a69067f283de2cd195f9f3
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2019-01-04 15:33:20 +0100

    cleaned code

--- public/index.js
+++ public/index.js
@@ -1,8 +1,7 @@
-const path = require('path');
 const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
 
-// start server!
+// start server
 app.listen((process.env.PORT || 5000));
