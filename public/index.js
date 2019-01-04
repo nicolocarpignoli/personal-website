0fbeee4c6a5f76052eea32ed315fe2e82591b149
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2019-01-04 09:53:58 +0100

    fixed public

--- public/index.js
+++ public/index.js
@@ -2,10 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-// Render the app
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'));
-});
+app.use(express.static(__dirname + '/public'));
 
 // start server!
 app.listen((process.env.PORT || 5000));
